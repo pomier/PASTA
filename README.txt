8d8bce3b376d250879abcf8d1c00ddf0259ed9c9
Rogdham <contact@rogdham.net>
2012-04-05 19:33:02 +0200

    Merging from what was stated in a pad.

---
+++ README.txt
@@ -0,0 +1,20 @@
+                             ____   _    ____ _____  _
+                            |  _ \ / \  / ___|_   _|/ \
+                            | |_) / _ \ \___ \ | | / _ \
+                            |  __/ ___ \ ___) || |/ ___ \
+                            |_| /_/   \_\____/ |_/_/   \_\
+                         PASTA is another SSH traffic analyser
+
+
+
+AUTHORS
+
+The PASTA team:
+    César 'Mr. Blue' Burini
+    Pierre 'Rogdham' Pavlidès
+    Romain 'Haradwaith' Pomier
+
+
+
+TODO: rest of the file (description of the software, requirements, how
+      to use, install, etc) - may be done later
---
+++ pasta/connection.py
@@ -0,0 +1,123 @@
+# Copyright (C) 2012 The PASTA team.
+# See the README file for the exhaustive list of authors.
+#
+# This file is part of PASTA.
+#
+# PASTA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PASTA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PASTA.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+class Connection:
+    'A SSH connection'
+
+    def __init__(self, datagrams, startTime, duration, clientIP,
+            serverIP, clientPort, serverPort, clientProtocol, serverProtocol):
+        self.datagrams = datagrams # list of Datagram instances
+        self.startTime = startTime # instance of datetime.datetime
+        self.duration = duration # instance of datetime.timedelta
+        self.clientIP = clientIP # string e.g. '123.234.0.42'
+        self.serverIP = serverIP # string e.g. '123.234.13.37'
+        self.clientPort = clientPort # int
+        self.serverPort = serverPort # int
+        self.clientProtocol = clientProtocol # string eg. "OpenSSH 5.3"
+        self.serverProtocol = serverProtocol # string eg. "OpenSSH 5.2"
+        self.clientSentNbDatagrams = sum(1 for p in self.datagrams if p.sentByClient)
+        self.serverSentNbDatagrams = sum(1 for p in self.datagrams \
+                if not p.sentByClient)
+        self.clientSentLen = sum(p.len for p in self.datagrams if p.sentByClient)
+        self.serverSentLen = sum(p.len for p in self.datagrams \
+                if not p.sentByClient)
+        self.idleTime = None # [Task3] e.g. 0.31415 (for 31.14%)
+        self.connexionType = None # [Task3] e.g. 'shell', 'ssh', 'tunnel'
+        #                           [Task4] other things
+        # TODO: other things for Task4
+
+    def __repr__(self):
+        s = '''
+Connection: %s:%d --> %s:%d
+Start date: %s
+Duration: %s
+Client: %s
+Server: %s
+Datagrams sent by client: %d (%d bytes)
+Datagrams sent by server: %d (%d bytes)
+''' % (self.clientIP, self.clientPort, self.serverIP, self.serverPort, \
+                self.startTime.strftime('%b %d, %Y - %H:%M:%S'), \
+                str(self.duration), # FIXME better representation? \
+                self.clientProtocol, self.serverProtocol, \
+                self.clientSentNbDatagrams, self.clientSentLen, \
+                self.serverSentNbDatagrams, self.serverSentLen)
+        if self.idleTime is not None:
+            s += 'Idle time: %.2f%%\n' % self.idleTime
+        if self.connexionType is not None:
+            s += 'Connexion type: %s\n' % self.connexionType
+        return s.strip()
+        
+
+    def __str__(self):
+        return repr(self)
+
+    def computeRTT(self):
+        'Set an approximate RTT for each datagram in self.datagrams'
+        # FIXME on fait un RTT moyen, ca serait bcp plus simple non ?
+        #       A voir si ya une grosse difference du "Idle" avec une moyenne
+        #       simple, ou un RTT flottant...
+        pass # TODO
+
+
+
+class Datagram:
+    'A datagram of a ssh connection'
+
+    def __init__(self, sentByClient, time, seqNb, payloadLen, ack):
+        self.sentByClient = sentByClient # True or False
+        self.time = time # instance of datetime.datetime
+        self.seqNb = seqNb #int
+        self.payloadLen = payloadLen # int length of the payload
+        self.ack = ack # int : -1 if not ACKed else seqnb of the datagram ACKed.
+        self.RTT = None # instance of datetime.timedelta
+
+    def __repr__(self):
+        s = '''
+Datagram sent by %s
+Time: %s
+Sequence number: %d
+Payload length: %d bytes
+''' % ('client' if self.sentByClient else 'server', \
+                self.time.strftime('%b %d, %Y - %H:%M:%S.%f'), \
+                self.seqNb,
+                self.payloadLen)
+        if self.ack > 0:
+            s += 'Sequence number of datagram ACKed: %d\n' % self.ack
+        if self.RTT is not None:
+            s += 'Estimate RTT: %s\n' % str(self.RTT) # FIXME better repr?
+        return s.strip()
+        
+
+    def __str__(self):
+        return repr(self)
+
+
+
+if __name__ == '__main__':
+
+    import unittest
+
+    class TestConnection(unittest.TestCase):
+        pass # TODO for computeRTT
+
+    class TestDatagram(unittest.TestCase):
+        pass # TODO if usefull
+
+    unittest.main()
