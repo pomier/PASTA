bb24a39ebb19afd0d01b4a92a7f8e0b46c13a6e2
Rogdham <contact@rogdham.net>
2012-04-15 20:54:37 +0200

    HUGE CHANGE in connection_type.py

--- pasta/connection_type.py
+++ pasta/connection_type.py
@@ -25,25 +25,20 @@ class ConnectionType():
     # Configuration constants
 
     # To be part of a shell interaction
-    shell_max_time_to_reply = 1.5 # max nb of RTTs
-    shell_min_replies = 0.5 # min ratio of replies
+    shell_max_time_to_reply = 0.7 # max nb of RTTs
+    shell_min_replies = 0.6 # min ratio of replies
+
+    # To be part of a reverse shell interaction
+    rshell_max_time_to_reply = 0.7 # max nb of RTTs
+    rshell_min_replies = 0.6 # min ratio of replies
 
     # To be part of a SCP connection
-    scp_min_asymetry1 = 0.95 # min asymetry if server sent more
-    scp_max_asymetry2 = 0.05 # max asymetry if client sent more
-
-    # To be part of a tunneled connection
-    tunnel_min_time_to_reply = 1.5 # min nb of RTTs
-    tunnel_max_time_to_reply = 9999999999 # max nb of RTTs
-    tunnel_min_replies = 0.5 # min ratio of replies
-    tunnel_min_asymetry1 = 0.55 # min asymetry if server sent more
-    tunnel_max_asymetry2 = 0.45 # max asymetry if client sent more
-    # TODO FIXME: it is almost as if there was nothing to check for tunnel
-    #             so maybe just make it the default case?
+    scp_min_asymetry = 0.95 # min asymetry if server sent more
+    rscp_max_asymetry = 0.05 # max asymetry if client sent more
 
     def __init__(self, connection):
         self.connection = connection
-        self.time_to_reply = []
+        self.time_to_reply = {True: [], False: []}
         self.ratio_server_sent = 0
         self.logger = logging.getLogger('Conn%dType' % connection.nb)
 
@@ -51,72 +46,65 @@ class ConnectionType():
         """Find the type of the ssh connection"""
         self.logger.info('Start computation')
 
-        possible_shell = True
-        possible_scp = True
-        possible_tunnel = True
-
-        # compute number of related replies
+        # compute time to reply
         self.compute_time_to_reply()
-        replies_total = len(self.time_to_reply)
-        if replies_total:
-            replies_shell = sum(1 for t in self.time_to_reply
-                                if t <= ConnectionType.shell_max_time_to_reply)
-            replies_tunnel = sum(1 for t in self.time_to_reply
-                                 if ConnectionType.tunnel_min_time_to_reply \
-                                    <= t \
-                                    <= ConnectionType.tunnel_max_time_to_reply)
-            ratio_shell = float(replies_shell) / float(replies_total)
-            ratio_tunnel = float(replies_tunnel) / float(replies_total)
-            self.logger.debug('Replies ratio for shell: %.2f'
-                              ' (min %.2f required)'
-                    % (ratio_shell, ConnectionType.shell_min_replies))
-            self.logger.debug('Replies ratio for tunnel: %.2f'
-                              ' (min %.2f required)'
-                    % (ratio_tunnel, ConnectionType.tunnel_min_replies))
-            possible_shell &= ratio_shell >= ConnectionType.shell_min_replies
-            possible_tunnel &= ratio_tunnel \
-                               >= ConnectionType.tunnel_min_replies
-        else:
-            self.logger.warning('No replies to get information from')
-            possible_shell = False
-            possible_tunnel = False
+
+        # shell (True) and reverse shell (False)
+        name = {True: 'shell', False: 'reverse shell'}
+        max_time_to_reply = {True: ConnectionType.shell_max_time_to_reply,
+                             False: ConnectionType.rshell_max_time_to_reply}
+        min_replies = {True: ConnectionType.shell_min_replies,
+                       False: ConnectionType.rshell_min_replies}
+        possible = {True: False, False: False} # defaults to False
+        for way in (True, False): # for both shell and reverse shell
+            if len(self.time_to_reply[way]): # is there replies in this way?
+                # consider only the replies below the threshold
+                replies_to_consider = sum(1 for t in self.time_to_reply[way]
+                                          if t <= max_time_to_reply[way])
+                replies_total = len(self.time_to_reply[way])
+                # compute the ratio
+                ratio = float(replies_to_consider) / float(replies_total)
+                self.logger.debug('Replies ratio for %s: %.2f'
+                                  ' (min %.2f required)'
+                        % (name[way], ratio, min_replies[way]))
+                # given the ratio, make the decision
+                possible[way] = ratio >= min_replies[way]
+        # recopy the decisions
+        possible_shell = possible[True]
+        possible_rshell = possible[False]
 
         # compute asymetry
         self.compute_asymetry()
+
+        # scp up (True) and scp down (False)
         if self.ratio_server_sent > 0.5:
-            self.logger.debug('Asymetry: server sent more than client')
-            self.logger.debug('Asymetry ratio for scp: %.2f'
+            # scp
+            self.logger.debug('Asymetry ratio for direct scp: %.2f'
                               ' (min %.2f required)' % (self.ratio_server_sent,
-                                  ConnectionType.scp_min_asymetry1))
-            self.logger.debug('Asymetry ratio for tunnel: %.2f'
-                              ' (min %.2f required)' % (self.ratio_server_sent,
-                                  ConnectionType.tunnel_min_asymetry1))
-            possible_scp &= self.ratio_server_sent \
-                            >= ConnectionType.scp_min_asymetry1
-            possible_tunnel &= self.ratio_server_sent \
-                               >= ConnectionType.tunnel_min_asymetry1
+                                  ConnectionType.scp_min_asymetry))
+            possible_scp = self.ratio_server_sent \
+                            >= ConnectionType.scp_min_asymetry
+            possible_rscp = False
         else:
-            self.logger.debug('Asymetry: client sent more than server')
-            self.logger.debug('Asymetry ratio for scp: %.2f'
+            # reverse scp
+            self.logger.debug('Asymetry ratio for reverse scp: %.2f'
                               ' (max %.2f required)' % (self.ratio_server_sent,
-                                  ConnectionType.scp_max_asymetry2))
-            self.logger.debug('Asymetry ratio for tunnel: %.2f'
-                              ' (max %.2f required)' % (self.ratio_server_sent,
-                                  ConnectionType.tunnel_max_asymetry2))
-            possible_scp &= self.ratio_server_sent \
-                            <= ConnectionType.scp_max_asymetry2
-            possible_tunnel &= self.ratio_server_sent \
-                               <= ConnectionType.tunnel_max_asymetry2
+                                  ConnectionType.rscp_max_asymetry))
+            possible_rscp = self.ratio_server_sent \
+                            <= ConnectionType.rscp_max_asymetry
+            possible_scp = False
 
         # choose connection type (order of the conditions is important)
         if possible_scp:
-            self.connection.connectionType = 'scp'
+            self.connection.connectionType = 'scp (up)'
+        elif possible_rscp:
+            self.connection.connectionType = 'scp (down)'
         elif possible_shell:
             self.connection.connectionType = 'shell'
-        elif possible_tunnel:
-            self.connection.connectionType = 'tunnel'
+        elif possible_rshell:
+            self.connection.connectionType = 'reverse shell'
         else:
-            self.logger.warning('Failed to find connection type')
+            self.connection.connectionType = 'tunnel'
 
         self.logger.info('Computations finished: type is %s'
                 % self.connection.connectionType)
@@ -134,21 +122,22 @@ class ConnectionType():
             self.ratio_server_sent = serverSent / (serverSent + clientSent)
 
     def compute_time_to_reply(self):
-        """Compute the times for the server to reply"""
-        self.time_to_reply = []
-        last_datagram = None
+        """Compute the times to reply"""
+        # True: time for the server to reply
+        # False: time for the client to reply
+        self.time_to_reply = {True: [], False: []}
+        last_datagram = {True: None, False: None}
         for datagram in self.connection.datagrams:
             if not datagram.payloadLen:
                 # no payload, skip
                 continue
-            if datagram.sentByClient:
-                # may create a reply
-                last_datagram = datagram
-            else:
-                if last_datagram is not None and datagram.RTT.total_seconds():
-                    # a reply
-                    self.time_to_reply.append(
-                        (datagram.time - last_datagram.time).total_seconds() /
-                        last_datagram.RTT.total_seconds()
-                        )
-                last_datagram = None
+            way = not datagram.sentByClient
+            if last_datagram[way] is not None \
+                    and last_datagram[way].RTT.total_seconds():
+                # a reply
+                self.time_to_reply[way].append(
+                    (datagram.time - last_datagram[way].time).total_seconds() /
+                    last_datagram[way].RTT.total_seconds()
+                    )
+            last_datagram[way] = None
+            last_datagram[not way] = datagram
